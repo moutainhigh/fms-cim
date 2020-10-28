e542996fe3eec55090b3ecd09a5294487bd785c7
czy <czy@DESKTOP-9LNOG4H>
2020-10-28 18:52:11 +0800

    1

--- fms-cim-server/src/main/java/org/fms/cim/server/App.java
+++ fms-cim-server/src/main/java/org/fms/cim/server/App.java
@@ -1,13 +1,19 @@
 package org.fms.cim.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@EnableEurekaClient
+@SpringBootApplication(scanBasePackages = "org.fms.cim", exclude = MongoAutoConfiguration.class)
+public class App {
+	public static void main(String[] args) {
+
+		SpringApplication.run(App.class, args);
+	}
 }
