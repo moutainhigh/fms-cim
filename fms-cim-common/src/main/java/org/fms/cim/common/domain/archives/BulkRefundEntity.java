60930df124f77ad47578da9313833352f5e322e8
czy <czy@DESKTOP-9LNOG4H>
2020-10-29 17:38:17 +0800

    版本变更

--- fms-cim-common/src/main/java/org/fms/cim/common/domain/archives/BulkRefundEntity.java
+++ fms-cim-common/src/main/java/org/fms/cim/common/domain/archives/BulkRefundEntity.java
@@ -2,29 +2,25 @@ package org.fms.cim.common.domain.archives;
 
 import java.math.BigDecimal;
 
-import com.alibaba.excel.annotation.ExcelProperty;
-import com.alibaba.excel.metadata.BaseRowModel;
+public class BulkRefundEntity {
 
-public class BulkRefundEntity extends BaseRowModel {
-    @ExcelProperty(value = "结算户号", index = 0)
-    private String settlementNo;
+	private String settlementNo;
 
-    @ExcelProperty(value = "退费金额", index = 1)
-    private BigDecimal refundMoney;
+	private BigDecimal refundMoney;
 
-    public String getSettlementNo() {
-        return settlementNo;
-    }
+	public String getSettlementNo() {
+		return settlementNo;
+	}
 
-    public void setSettlementNo(String settlementNo) {
-        this.settlementNo = settlementNo;
-    }
+	public void setSettlementNo(String settlementNo) {
+		this.settlementNo = settlementNo;
+	}
 
-    public BigDecimal getRefundMoney() {
-        return refundMoney;
-    }
+	public BigDecimal getRefundMoney() {
+		return refundMoney;
+	}
 
-    public void setRefundMoney(BigDecimal refundMoney) {
-        this.refundMoney = refundMoney;
-    }
+	public void setRefundMoney(BigDecimal refundMoney) {
+		this.refundMoney = refundMoney;
+	}
 }
