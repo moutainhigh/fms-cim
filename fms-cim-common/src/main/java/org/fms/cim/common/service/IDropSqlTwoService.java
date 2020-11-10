5e7a761a25d25f10a1e9ac1ceeba684cc963f133
t1mmothy-404 <t1mothy404@163.com>
2020-11-10 17:42:28 +0800

    DROP_SQL

--- fms-cim-common/src/main/java/org/fms/cim/common/service/IDropSqlTwoService.java
+++
@@ -1,9 +0,0 @@
-package org.fms.cim.common.service;
-
-import java.util.List;
-
-import org.fms.cim.common.domain.archives.DropSqlDomain;
-
-public interface IDropSqlTwoService{
-	public List<DropSqlDomain> findByWhere(DropSqlDomain dropSqlDomain);
-}
