470d2aa1a142fe1c97b53262acb09b832500ac71
czy <czy@DESKTOP-9LNOG4H>
2020-11-12 18:34:13 +0800

    delete celue

--- fms-cim-common/src/main/java/org/fms/cim/common/strategy/no/SequenceStrategy.java
+++
@@ -1,6 +0,0 @@
-package org.fms.cim.common.strategy.no;
-
-public interface SequenceStrategy {
-    public String generateSequenceNo(String condition);
-    public String generateSequenceNo(String condition, int size);
-}
